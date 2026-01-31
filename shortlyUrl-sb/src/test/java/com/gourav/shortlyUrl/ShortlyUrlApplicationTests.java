8baa9ef32cebdb2cf92765fb0a9e6c7a9a9fa4e9
Gourav Chouhan <gouravchouhan2512@gmail.com>
2026-01-31 07:51:56 +0530

    Initial commits

---
+++ shortlyUrl-sb/src/test/java/com/gourav/shortlyUrl/ShortlyUrlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gourav.shortlyUrl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShortlyUrlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
